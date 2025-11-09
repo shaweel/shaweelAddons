79e51a674b8fbacc4ba417c488ca003829e4b4dc
shaweel <152102143+shaweel@users.noreply.github.com>
2025-11-09 11:47:42 +0100

    Delete features directory

--- features/CrystalAlert.js
+++
@@ -1,7 +0,0 @@
-import Settings from "../config.js"
-
-register("chat", (plr) => {
-    if (plr != Player.getName()) return
-    if (Settings.crystalSound) utils.playSound("note.pling", 1, 2)
-    if (Settings.crystalTitle) Client.showTitle(Settings.crystalText, "", "10", "10", "10")
-}).setCriteria("${plr} picked up an Energy Crystal!")
