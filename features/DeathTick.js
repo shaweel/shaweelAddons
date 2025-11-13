7b55f059e78f1c3eecc25edaf25dd4cde15ad1cc
shaweel <152102143+shaweel@users.noreply.github.com>
2025-11-13 06:10:11 +0100

    Update DeathTick.js

--- features/DeathTick.js
+++ features/DeathTick.js
@@ -1,25 +1,98 @@
 import utils from "../utils.js"
 import Settings from "../config.js"
+import { getCurrentSplit } from "./Splits.js"
 import { setLine, setShouldRender } from "./Gui.js"
 
-let timer = 2
-register("worldLoad", () => {
-    if (!Settings.tick) return
-    timer = 1
-})
-
-
-register("packetReceived", () => {
-    if (!Settings.tick) return
-    if (utils.getDungeonFloor == -1) {
+let timer = -1
+register("packetReceived", (p) => {
+    if (utils.getDungeonFloor() == -1) {
         setShouldRender(3, false)
         return
     }
-    timer -= 0.05
-    setShouldRender(3, true)
-    setLine(3, 0, "&6Tick: &f"+utils.formatSmallNumber(timer, 2)+"s")
-    if (timer <= 0) {
-        timer = 2
-        utils.playSound("note.pling", 1, 2)
+    if (String(p).includes("S32PacketConfirmTransaction")) {
+        if (timer > 0) {
+            timer = Math.max(0, timer - 0.05)
+        } else {
+            if (getCurrentSplit() == "Nothing") {
+                timer = 2
+                utils.playSound("note.pling", 1, 2)
+            } else {
+                timer = 1
+            }
+        }
     }
-}).setFilteredClass(net.minecraft.network.play.server.S32PacketConfirmTransaction)
+    if (getCurrentSplit() == "Enter") {
+        setShouldRender(3, true)
+        if (!Settings.secretTick) {
+            setShouldRender(3, false)
+            return
+        }
+        setShouldRender(3, true)
+        if (timer > 13/20) {
+            setLine(3, 0, "&7Secret: &a"+utils.formatSmallNumber(timer, 2)+"s")
+        } else if (timer > 6/20) {
+            setLine(3, 0, "&7Secret: &6"+utils.formatSmallNumber(timer, 2)+"s")
+        } else {
+            setLine(3, 0, "&7Secret: &c"+utils.formatSmallNumber(timer, 2)+"s")
+        }
+        return
+    }
+    if (getCurrentSplit() != "Nothing") {
+        setShouldRender(3, false)
+        return
+    }
+    if (!Settings.outTick) {
+        setShouldRender(3, false)
+        return
+    }
+    setShouldRender(3, true)
+    if (timer > 13/20) {
+        setLine(3, 0, "&8Outbounds: &a"+utils.formatSmallNumber(timer, 2)+"s")
+    } else if (timer > 6/20) {
+        setLine(3, 0, "&8Outbounds: &6"+utils.formatSmallNumber(timer, 2)+"s")
+    } else {
+        setLine(3, 0, "&8Outbounds: &c"+utils.formatSmallNumber(timer, 2)+"s")
+    }
+
+    if (!String(p).includes("S03PacketTimeUpdate")) return
+    if (!Settings.tick) return
+    if (utils.getDungeonFloor() == -1) {
+        setShouldRender(3, false)
+        return
+    }
+    if (getCurrentSplit() == "Enter") {
+        setShouldRender(3, true)
+        timer = (20 - (p.func_149366_c() % 20))/20
+        if (!Settings.secretTick) {
+            setShouldRender(3, false)
+            return
+        }
+        setShouldRender(3, true)
+        if (timer > 13/20) {
+            setLine(3, 0, "&7Secret: &a"+utils.formatSmallNumber(timer, 2)+"s")
+        } else if (timer > 6/20) {
+            setLine(3, 0, "&7Secret: &6"+utils.formatSmallNumber(timer, 2)+"s")
+        } else {
+            setLine(3, 0, "&7Secret: &c"+utils.formatSmallNumber(timer, 2)+"s")
+        }
+        return
+    }
+    if (getCurrentSplit() != "Nothing") {
+        setShouldRender(3, false)
+        return
+    }
+    timer = (40 - (p.func_149366_c() % 40))/20
+    if (!Settings.outTick) {
+        setShouldRender(3, false)
+        return
+    }
+    if (timer == 20) utils.playSound("note.pling", 1, 2)
+    setShouldRender(3, true)
+    if (timer > 13/20) {
+        setLine(3, 0, "&8Outbounds: &a"+utils.formatSmallNumber(timer, 2)+"s")
+    } else if (timer > 6/20) {
+        setLine(3, 0, "&8Outbounds: &6"+utils.formatSmallNumber(timer, 2)+"s")
+    } else {
+        setLine(3, 0, "&8Outbounds: &c"+utils.formatSmallNumber(timer, 2)+"s")
+    }
+})
