2efe2836ffc60a4a6dc46ca07282306c81e3a49d
shaweel <152102143+shaweel@users.noreply.github.com>
2025-11-13 06:08:55 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -186,6 +186,7 @@ class utils {
         if (assignIndex == 0) return "katanaHud"
         if (assignIndex == 1) return "Splits"
         if (assignIndex == 2) return "chestProfit"
+        if (assignIndex == 3) return "tick"
         console.error("shaweel is stupid and forgot to asign a gui to the index, blame shaweel(assignElementName)("+assignIndex+")")
     }
     
@@ -193,6 +194,7 @@ class utils {
         if (name == "katanaHud") return 0
         if (name == "Splits") return 1
         if (name == "chestProfit") return 2
+        if (name == "tick") return 3
         console.error("shaweel is stupid and forgot to asign a gui to the index, blame shaweel(getIndexFromName)("+name+")")
     }
 
