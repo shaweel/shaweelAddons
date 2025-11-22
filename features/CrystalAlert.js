9ebc7cd64416aaff3d7807210fba6c4e44012b14
shaweel <152102143+shaweel@users.noreply.github.com>
2025-11-22 14:14:07 +0100

    Update CrystalAlert.js

--- features/CrystalAlert.js
+++ features/CrystalAlert.js
@@ -1,7 +1,8 @@
 import Settings from "../config.js"
+import utils from "../utils.js"
 
 register("chat", (plr) => {
-    if (plr != Player.getName()) return
-    if (Settings.crystalSound) utils.playSound("note.pling", 1, 2)
-    if (Settings.crystalTitle) Client.showTitle(Settings.crystalText, "", "10", "10", "10")
+    if (ChatLib.removeFormatting(plr) != ChatLib.removeFormatting(Player.getName())) return
+    if (Settings.crystalSound) {utils.playSound("note.pling", 1, 2)}
+    if (Settings.crystalTitle) {Client.showTitle(Settings.crystalText, "", "10", "10", "10")}
 }).setCriteria("${plr} picked up an Energy Crystal!")
