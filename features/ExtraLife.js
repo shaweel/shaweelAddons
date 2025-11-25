fdb7a90f43d6b287a404ba965b639853c2050afd
shaweel <152102143+shaweel@users.noreply.github.com>
2025-11-25 19:22:51 +0100

    Update ExtraLife.js

--- features/ExtraLife.js
+++ features/ExtraLife.js
@@ -2,29 +2,37 @@ import Settings from "../config.js"
 import utils from "../utils.js"
 
 function registerExtraLifeItem(chatMessage, title, text, sound) {
-    register("chat", () => {
-        if (Settings[sound]) utils.playSound("note.pling", 1, 2)
-        if (Settings[title]) Client.showTitle(Settings[text], "", "10", "10", "10")
-    }).setCriteria(chatMessage)
+	register("chat", () => {
+		sinceLastBonzo = Date.now()
+		if (Settings[sound]) utils.playSound("note.pling", 1, 2) 
+		if (Settings[title]) utils.drawCustomText(Settings[text], 500, 500, 500)
+	}).setCriteria(chatMessage)
 }
 
 registerExtraLifeItem(
-    "Your Bonzo's Mask saved your life!",
-    "bonzoTitle",
-    "bonzoText",
-    "bonzoSound",
+	"Your Bonzo's Mask saved your life!",
+	"bonzoTitle",
+	"bonzoText",
+	"bonzoSound",
 )
 
 registerExtraLifeItem(
-    "Second Wind Activated! Your Spirit Mask saved your life!",
-    "spiritTitle",
-    "spiritText",
-    "spiritSound",
+	"Your ⚚ Bonzo's Mask saved your life!",
+	"bonzoTitle",
+	"bonzoText",
+	"bonzoSound",
 )
 
 registerExtraLifeItem(
-    "Your Phoenix Pet saved you from certain death!",
-    "phoenixTitle",
-    "phoenixText",
-    "phoenixSound",
+	"Second Wind Activated! Your Spirit Mask saved your life!",
+	"spiritTitle",
+	"spiritText",
+	"spiritSound",
+)
+
+registerExtraLifeItem(
+	"Your Phoenix Pet saved you from certain death!",
+	"phoenixTitle",
+	"phoenixText",
+	"phoenixSound",
 )
